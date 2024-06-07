1b45a5ecb15f73bb5634287cf21a9d37ce4f9735
Omar Cajusol <84242969+CajuFal@users.noreply.github.com>
2024-06-07 01:36:20 +0000

    delte member.js

--- member.js
+++
@@ -1,8 +0,0 @@
-function skillsMember() {
-  var member = document.getElementById('skillsMember');
-  if (member.style.display === 'none') {
-    member.style.display = 'block';
-  } else {
-    member.style.display = 'none';
-  }
-}
